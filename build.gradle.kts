08d73886c7f671a76a11c4665c3b9ed46cdc652b
LSO <laurent.soulet@gmail.com>
2024-09-17 22:27:04 +0200

    v2024.21: compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,68 +1,87 @@
+import org.jetbrains.intellij.platform.gradle.IntelliJPlatformType
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+import org.jetbrains.intellij.platform.gradle.models.ProductRelease
+import org.jetbrains.intellij.platform.gradle.tasks.VerifyPluginTask
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
   // Java support
   id("java")
   // Kotlin support
-  id("org.jetbrains.kotlin.jvm") version "2.0.0-Beta3"
+  id("org.jetbrains.kotlin.jvm") version "2.0.20-RC2"
   // gradle-intellij-plugin - read more: https://github.com/JetBrains/gradle-intellij-plugin
-  id("org.jetbrains.intellij") version "1.17.1"
+  id("org.jetbrains.intellij.platform") version "2.0.1"
 }
 
-group = "org.lso"
-version = "v2024.2"
+tasks.withType<KotlinCompile> {
+  compilerOptions {
+    jvmTarget.set(JvmTarget.JVM_21)
+  }
+}
 
 // Configure project's dependencies
 repositories {
   mavenCentral()
+
+  intellijPlatform {
+    defaultRepositories()
+  }
+}
+
+dependencies {
+  intellijPlatform {
+    intellijIdeaUltimate("2024.2")
+    bundledPlugin("JavaScript")
+    instrumentationTools()
+    pluginVerifier()
+    testFramework(TestFrameworkType.Platform)
+  }
+  testImplementation("junit:junit:4.13.2")
+  testImplementation("org.opentest4j:opentest4j:1.3.0")
 }
 
 // Configure gradle-intellij-plugin plugin.
 // Read more: https://github.com/JetBrains/gradle-intellij-plugin
-intellij {
-  pluginName.set("LogIt")
-
-  // see https://www.jetbrains.com/intellij-repository/releases/
-  // and https://www.jetbrains.com/intellij-repository/snapshots/
-  version.set("2024.1")
-  type.set("IU")
-
-  downloadSources.set(!System.getenv().containsKey("CI"))
-  updateSinceUntilBuild.set(true)
-
-  // Plugin Dependencies -> https://plugins.jetbrains.com/docs/intellij/plugin-dependencies.html
-  // Example: platformPlugins = com.intellij.java, com.jetbrains.php:203.4449.22
-  //
-  plugins.set(listOf("JavaScript"))
-
-  sandboxDir.set(project.rootDir.canonicalPath + "/.sandbox")
-}
-
-tasks {
-  // Set the compatibility versions to 1.8
-  withType<JavaCompile> {
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
+intellijPlatform {
+  pluginConfiguration {
+    group = "org.lso"
+    name.set("LogIt")
+    version.set("2024.21")
   }
-  withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "17"
+  pluginVerification {
+    failureLevel = VerifyPluginTask.FailureLevel.ALL
+    verificationReportsDirectory = file("build/reports/pluginVerifier")
+    verificationReportsFormats = VerifyPluginTask.VerificationReportsFormats.ALL
+    teamCityOutputFormat = false
+    subsystemsToCheck = VerifyPluginTask.Subsystems.ALL
+    ides {
+      ide(IntelliJPlatformType.WebStorm, "2024.2")
+      recommended()
+      select {
+        types = listOf(IntelliJPlatformType.WebStorm)
+        channels = listOf(ProductRelease.Channel.RELEASE)
+        sinceBuild = "232"
+        untilBuild = "242.*"
+      }
+    }
   }
 
-  patchPluginXml {
-    sinceBuild.set("233")
-  }
-
-  publishPlugin {
+  publishing {
     token.set(System.getenv("ORG_GRADLE_PROJECT_intellijPublishToken"))
   }
 
-  runIde {
-    ideDir.set(file("/Users/laurent/Applications/WebStorm.app/Contents/"))
-  }
+  tasks {
 
-  patchPluginXml {
-    changeNotes.set(
-      """<br>
+    withType<JavaCompile> {
+      sourceCompatibility = "21"
+      targetCompatibility = "21"
+    }
+
+    patchPluginXml {
+      changeNotes.set(
+        """<br>
+      v2024.21 - compatibility with 2024.202 version<br>
       v2024.2 - compatibility with 2024.2 version<br>
       v2024.1 - compatibility with 2024.1 version<br>
       v2023.3 - compatibility with 2023.3 version<br>
@@ -80,7 +99,8 @@ tasks {
       v193.3.1 - some corrections when inserting a console.log from an empty line.<br>
       v1.0 - initial release.<br>
 """
-    )
+      )
+    }
   }
 }
 
